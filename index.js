85ddc37d0cd56844c7ace5fe25f72d15ef267381
Kevin Chappell <kevin.b.chappell@gmail.com>
2019-05-26 16:47:03 -0700

    fix: typo, auto release

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const { default: mi18n } = require('mi18n')
 
 const langFiles = fs
   .readdirSync(__dirname)
-  .filter(file => /.lang$/.test(file))
   .reduce((acc, lang) => {
+    if (!/.lang$/.test(lang)) {
+      return acc
+    }
     const langFile = fs.readFileSync(path.resolve(__dirname, lang)).toString()
     const fileName = path.basename(lang)
     const locale = fileName.substr(0, fileName.indexOf('.'))
