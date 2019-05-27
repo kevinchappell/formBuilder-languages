eea327dac9d6d3d7f9f303eb7bad0f49f18f2eb8
Kevin Chappell <kevin.b.chappell@gmail.com>
2019-05-26 17:02:42 -0700

    fix: typo, auto release (#39)
    * fix: typo, auto release
    
    * fix: add eslint
    
    * fix: remove build command

---
+++ test/index.spec.js
@@ -0,0 +1,42 @@
+const assert = require('assert')
+const langFiles = require('../index')
+describe('langFiles', function () {
+  it('should contain 27 languages', function () {
+    assert.equal(Object.keys(langFiles).length, 27)
+  })
+  it('should contain specific languages', function () {
+    const expected = [
+      'ar-SA',
+      'ar-TN',
+      'cs-CZ',
+      'de-DE',
+      'en-US',
+      'es-ES',
+      'fa-IR',
+      'fi-FI',
+      'fr-FR',
+      'he-IL',
+      'hu-HU',
+      'it-IT',
+      'ja-JP',
+      'my-MM',
+      'nb-NO',
+      'nl-NL',
+      'pl-PL',
+      'pt-BR',
+      'qz-MM',
+      'ro-RO',
+      'ru-RU',
+      'sl-SI',
+      'tr-TR',
+      'uk-UA',
+      'vi-VN',
+      'zh-CN',
+      'zh-TW'
+    ]
+
+    expected.forEach(lang => {
+      assert.equal(langFiles.hasOwnProperty(lang), true)
+    })
+  })
+})
