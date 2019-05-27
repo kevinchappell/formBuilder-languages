d191268a8fabd3d8eaae0c0fc5bc3c1cf54bf24e
Kevin Chappell <kevin.b.chappell@gmail.com>
2019-05-26 17:14:02 -0700

    fix: tests to include recently added Danish

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,14 +1,18 @@
 const assert = require('assert')
 const langFiles = require('../index')
+
+const languageCount = 28
+
 describe('langFiles', function () {
-  it('should contain 27 languages', function () {
-    assert.equal(Object.keys(langFiles).length, 27)
+  it(`should contain ${languageCount} languages`, function () {
+    assert.equal(Object.keys(langFiles).length, languageCount)
   })
   it('should contain specific languages', function () {
     const expected = [
       'ar-SA',
       'ar-TN',
       'cs-CZ',
+      'da-DK',
       'de-DE',
       'en-US',
       'es-ES',
