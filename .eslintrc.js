6b0e24134ca2a45d8eab3a2f935c76405e78eaf7
Kevin Chappell <kevin.b.chappell@gmail.com>
2020-08-22 11:53:34 -0700

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,17 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'commonjs': true,
-    'es6': true
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
   },
-  'extends': 'standard',
-  'globals': {
-    'Atomics': 'readonly',
-    'SharedArrayBuffer': 'readonly'
+  plugins: ['standard', 'plugin:mocha/recommended'],
+  extends: ['mocha'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
-  'parserOptions': {
-    'ecmaVersion': 2018
+  parserOptions: {
+    ecmaVersion: 2018,
   },
-  'rules': {
-  }
+  rules: {},
 }
