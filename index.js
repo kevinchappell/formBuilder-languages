9fef561b1ecb46a09cf25daf1887d296daa56657
Kevin Chappell <kevin.b.chappell@gmail.com>
2024-05-27 17:09:06 -0700

    fix: tests, remove old dependencies (#103)
    * fix: tests, remove old dependencies
    
    * chore: update gh action
    
    * chore: update publish
    
    * chore: use pnpm cache

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('node:fs')
+const path = require('node:path')
 const { default: mi18n } = require('mi18n')
 
 const langFiles = fs.readdirSync(__dirname).reduce((acc, lang) => {
@@ -8,7 +8,7 @@ const langFiles = fs.readdirSync(__dirname).reduce((acc, lang) => {
   }
   const langFile = fs.readFileSync(path.resolve(__dirname, lang)).toString()
   const fileName = path.basename(lang)
-  const locale = fileName.substr(0, fileName.indexOf('.'))
+  const locale = fileName.substring(0, fileName.indexOf('.'))
   acc[locale] = mi18n.processFile(langFile)
   return acc
 }, {})
---
+++ index.test.js
@@ -0,0 +1,25 @@
+const { test } = require('node:test')
+const assert = require('node:assert')
+const { glob } = require('node:fs')
+
+const langFiles = require('./index')
+
+glob('*.lang', (err, matches) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  const localeCount = matches.length
+
+  test(`should contain ${localeCount} languages`, () => {
+    assert.equal(Object.keys(langFiles).length, localeCount)
+  })
+
+  test('should contain specific languages', () => {
+    const expectedLocales = matches.map((lang) => lang.substring(0, lang.indexOf('.')))
+
+    for (const lang of expectedLocales) {
+      assert.equal(Object.hasOwn(langFiles, lang), true)
+    }
+  })
+})
